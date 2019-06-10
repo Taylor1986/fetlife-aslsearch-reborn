a692a821f6b53efd979d05bc317e9385c8047042
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2019-06-10 14:00:53 +0200

    Fixed rename directly

--- fetlife-age-sex-location-search.user.js
+++ fetlife-age-sex-location-search.user.js
@@ -7,7 +7,7 @@
 // @name           FetLife ASL Search (Extened Edition)
 // @version        0.4.6
 // @namespace      http://maybemaimed.com/playground/fetlife-aslsearch/
-// @updateURL      https://github.com/Ornias1993/fetlife-aslsearch/raw/master/fetlife-age-sex-location-search.user.js
+// @updateURL      https://github.com/Ornias1993/fetlife-aslsearch-reborn/raw/master/fetlife-age-sex-location-search.user.js
 // @description    Allows you to search for FetLife profiles based on age, sex, location, and role.
 // @require        https://code.jquery.com/jquery-2.1.4.min.js
 // @include        https://fetlife.com/*
